2896b86de7e6feeb619fa1ee27724973d200f306
Mox <tenmaken246@gmail.com>
2025-01-30 23:39:44 +0300

    нашла благ и улучшила ГИ

--- Main.py
+++ Main.py
@@ -45,32 +45,70 @@ class User:
     def close(self):
         self.connection.close()
 
+def delete_task():
+    for selected_item in tree.selection():
+        item = tree.set(selected_item, '#1')
+        I.cursor.execute('DELETE FROM Task WHERE task=?', (item,))
+        row = tree.item(selected_item)
+        I.tasks.remove(tuple(row["values"]))
+    view_records()
+    I.commit()
 
+def add_task():
+    new_task = Task(entryTask.get(), datetime.strptime(entryDate.get(), '%d-%m-%Y %H:%M'))
+    new_task.save_task()
+    I.commit()
+    I.tasks = I.select()
+    view_records()
+
+def view_records():
+    for i in tree.get_children():
+        tree.delete(i)
+    for task in I.tasks:
+        tree.insert("", END, values=task)
 
 I = User()
-# date_time = "30/01/25 17:00"
-# first_task = Task("дз ", datetime.strptime(date_time, "%d/%m/%y %H:%M"))
-# first_task.save_task()
-# I.commit()
 
 window = Tk()
 window.title("To Do List")
 window.geometry('600x400')
 
 columns = ("task", "data", "time")
-tasks = I.select()
+I.tasks = I.select()
 
 tree = ttk.Treeview(columns=columns, show="headings")
-tree.pack(fill=BOTH, expand=1)
+tree.grid(row=3, column=0, sticky="nsew")
 
-# определяем заголовки
+btn = Button(window, text="Удалить", command=delete_task)
+btn.grid(row=2,column=1)
+
+btn2 = Button(window, text="Добавить", command=add_task)
+btn2.grid(row=2,column=0)
+
+task_label = ttk.Label(window, text="Задание:")
+entryTask = ttk.Entry()
+task_label.grid(row=0, column=0)
+entryTask.grid(row=0, column=1)
+
+date_label = ttk.Label(window, text="День и Время ('%d-%m-%Y %H:%M'):")
+entryDate = ttk.Entry()
+date_label.grid(row=1, column=0)
+entryDate.grid(row=1, column=1)
+
 tree.heading("task", text="Задание")
-tree.heading("data", text="день")
-tree.heading("time", text="время")
+tree.heading("data", text="День")
+tree.heading("time", text="Время")
 
-# добавляем данные
-for task in tasks:
-    tree.insert("", END, values=task)
+#добавить ещё одну колонку с ID, чтобы при удалении не удалялись все одинаковые задачи
+tree.column("#1", stretch=True,  width=300)
+tree.column("#2", stretch=NO, width=70)
+tree.column("#3", stretch=NO, width=70)
+
+view_records()
+
+scrollbar = ttk.Scrollbar(orient=VERTICAL, command=tree.yview)
+tree.configure(yscroll=scrollbar.set)
+scrollbar.grid(row=3, column=1, sticky="ns")
 
 window.mainloop()
 I.close()
